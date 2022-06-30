7dd8bd5a65c35f0d96cfbe37d678475e528de391
komisssarova <komisssarova.uk@gmail.com>
2022-06-30 12:12:28 +0100

    created react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        Hello, world!
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
