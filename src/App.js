845a4691b7a565cfb887986197df55713f01ccc6
komisssarova <komisssarova.uk@gmail.com>
2022-07-21 20:13:26 +0100

    removed and changed few things

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import About from "./About";
 import Projects from "./Projects";
 import "./App.css";
 
-
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid"> <span>
-           <a href="/" title="Homepage">Home</a> {""}
-           <a href="/" title="About">About</a> {""}
-           <a href="/" title="Work" >Work</a> {""}
-           <a href="/" title="Contact">Contact</a> </span>
+        <nav className="d-flex justify-content-beetween">
+          <div className="container-container container">
+            {" "}
+            <span>
+              <a href="./App.js" title="Homepage">
+                Home
+              </a>{" "}
+              {""}
+              <a href="./About" title="About">
+                About
+              </a>{" "}
+              {""}
+              <a href="/" title="Work">
+                Work
+              </a>{" "}
+              {""}
+              <a href="/" title="Contact">
+                Contact
+              </a>{" "}
+            </span>
           </div>
         </nav>
       </header>
-        <div><About /></div>
-        <div><Projects /></div>
+      <div>
+        <About />
+      </div>
+      <div>
+        <Projects />
+      </div>
     </div>
   );
 }
